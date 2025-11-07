0671c554c609806ff3744107820d83ad54824c6a
quyansiyuanwang <2301179475@qq.com>
2025-11-07 23:39:40 +0800

    chore(tool): import sorted

--- tools/pre_cmt.py
+++ tools/pre_cmt.py
@@ -1,8 +1,8 @@
 """[tools]预提交脚本"""
 
 from scripts.compile_ui import main as compile_ui_main
-from tools.import_sort import main as import_sort_main
 from tools.code_format import main as code_format_main
+from tools.import_sort import main as import_sort_main
 from tools.type_check import main as type_check_main
 
 
